b78a0da7e2e2e546799b01d72fa8926b89c086a4
IBRHMHMDY <ibrhmhmdy@gmail.com>
2025-07-07 12:08:11 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_app_ui/screens/app_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Furniture Shop',
+      theme: ThemeData(),
+      home: AppScreen(),
+    );
+  }
+}
---
+++ lib/screens/app_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_app_ui/screens/details_screen.dart';
+import 'package:furniture_app_ui/screens/home_screen.dart';
+import 'package:furniture_app_ui/screens/onboarding_screen.dart';
+
+class AppScreen extends StatelessWidget {
+  AppScreen({super.key});
+  final int selectedScreen = 0;
+  final List screens = [OnBoardingScreen(), HomeScreen(), DetailsScreen()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[selectedScreen],
+    );
+  }
+}
